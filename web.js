8119c23d857985103fc3e9e2a9ebc822fc52e71e
caiodsc <caio.dscamara@gmail.com>
2020-05-23 12:08:59 -0400

    Proxy

--- web.js
+++ web.js
@@ -5,14 +5,23 @@ var newBaseURL = process.env.NEW_BASE_URL || 'http://example.com';
 var redirectStatus = parseInt(process.env.REDIRECT_STATUS || 302);
 var port = process.env.PORT || 5000;
 
-app.get('*', function(request, response) {
-  response.redirect(redirectStatus, newBaseURL + request.url);
+var http = require('http'),
+    httpProxy = require('http-proxy');
+
+//
+// Create a proxy server with custom application logic
+//
+var proxy = httpProxy.createProxyServer({});
+
+//
+// Create your custom server and just call `proxy.web()` to proxy
+// a web request to the target passed in the options
+// also you can use `proxy.ws()` to proxy a websockets request
+//
+var server = http.createServer(function(req, res) {
+  // You can define here your custom logic to handle the request
+  // and then proxy the request.
+  proxy.web(req, res, { target: newBaseURL });
 });
 
-app.post('*', function(request, response) {
-  response.redirect(307, newBaseURL + request.url);
-});
-
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+server.listen(port);
