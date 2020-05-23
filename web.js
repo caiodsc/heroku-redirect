842491584e20f46b8f1bb49517e7b194256334b1
caiodsc <caio.dscamara@gmail.com>
2020-05-23 15:07:58 -0400

    Proxy

--- web.js
+++ web.js
@@ -21,8 +21,7 @@ var proxy = httpProxy.createProxyServer({});
 var server = http.createServer(function(req, res) {
   // You can define here your custom logic to handle the request
   // and then proxy the request.
-  proxy.web(req, res, { target: 'http://pavlok-redirected.herokuapp.com:80' });
+  proxy.web(req, res, { target: 'https://pavlok-redirected.herokuapp.com' });
 });
 
-console.log("listening on port 5050")
-server.listen(5000);
+server.listen(port);
